aa0f3ba398c6ab56432014729bdf22dcd9968066
Ismail Koksal <ismail@koksal.fr>
2019-07-03 19:40:33 +0200

    Create & add PlayerController script on player GameObject

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movespeed = 12f;     // How fast the player moves forward and back.
+    [SerializeField] private float turnSpeed = 180f;    // How fast the player turns in degrees per second.
+
+    private Rigidbody rb;                               // Reference used to move the player.
+    private float moveHorizontal;
+    private float moveVertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Store the value of both input axes.
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        // Adjust the rigidbody's position and orientation in FixedUpdate.
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        // Create a vector in the direction the player is facing with a magnitude based on the input, speed and the time between frames.
+        Vector3 movement = transform.forward * moveVertical * movespeed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        rb.MovePosition(rb.position + movement);
+    }
+
+    private void Turn()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = moveHorizontal * turnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+}
